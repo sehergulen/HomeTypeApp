3192ef757e87a89f73e1f5cddb17d3b612290af3
sehergulen <sehergulen1@gmail.com>
2023-01-29 16:13:49 +0300

    added CommandLineRunner

--- src/main/java/com/homework/one/OneApplication.java
+++ src/main/java/com/homework/one/OneApplication.java
@@ -1,12 +1,25 @@
 package com.homework.one;
 
+import com.homework.one.service.HomeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OneApplication {
+@Slf4j
+
+public class OneApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(OneApplication.class, args);
 	}
+	@Autowired
+	HomeService homeService;
+	@Override
+	public void run(String... args) throws Exception {
 
+		log.info("{}",homeService.calculateTotalPricesOfHomes());
+
+	}
 }
